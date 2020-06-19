d45eacce7af7c49278d28b45a4eddd2f5b48f9af
eco9898 <eco9898@live.com>
2020-06-19 13:02:51 +1000

    feature: added gender selection

--- Scenes/CharacterCreation/CharacterCreation.gd
+++ Scenes/CharacterCreation/CharacterCreation.gd
@@ -6,7 +6,7 @@ var battle_scene = load("res://Scenes/Battle/Battle.tscn").instance()
 onready var selected_character := -1
 onready var selected_equip := -1
 const profiles := ["res://Assets/Images/Profiles/Friendlies/Tex_AnimeAva_01.png", "res://Assets/Images/Profiles/Friendlies/Tex_AnimeAva_17.png", "res://Assets/Images/Profiles/Friendlies/Tex_AnimeAva_28.png", "res://Assets/Images/Profiles/Friendlies/Tex_AnimeAva_51.png"]
-
+onready var gender := ""
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Set button text to disabled state
@@ -31,26 +31,26 @@ func _ready():
 	ial[0].get_node("Labels/Label").text = "Intelligence"
 	ial[1].get_node("Labels/Label").text = "Agility"
 	ial[2].get_node("Labels/Label").text = "Luck"
-	var knight = get_node("MainMenu/Choices/Equipment/Classes/Knight")
+	var knight = get_node("MainMenu/Choices/Class/Classes/Knight")
 	knight.get_node("ClassName").text = "Knight"
 	knight.get_node("ImgCenter/ClassImg").texture = load("res://Assets/Images/Icons/Classes/Badge_warrior.png")
 
-	var berserker = get_node("MainMenu/Choices/Equipment/Classes/Berserker")
+	var berserker = get_node("MainMenu/Choices/Class/Classes/Berserker")
 	berserker.get_node("ClassName").text = "Berserker"
 	berserker.get_node("ImgCenter/ClassImg").texture = load("res://Assets/Images/Icons/Classes/Badge_barbarian.png")
 
-	var battle_mage = get_node("MainMenu/Choices/Equipment/Classes/BattleMage")
+	var battle_mage = get_node("MainMenu/Choices/Class/Classes/BattleMage")
 	battle_mage.get_node("ClassName").text = "Battle Mage"
 	battle_mage.get_node("ImgCenter/ClassImg").texture = load("res://Assets/Images/Icons/Classes/Badge_mage.png")
 
-	var quick_shooter = get_node("MainMenu/Choices/Equipment/Classes/QuickShooter")
+	var quick_shooter = get_node("MainMenu/Choices/Class/Classes/QuickShooter")
 	quick_shooter.get_node("ClassName").text = "Quick Shooter"
 	quick_shooter.get_node("ImgCenter/ClassImg").texture = load("res://Assets/Images/Icons/Classes/Badge_hunter.PNG")
 
 	Core.emit_signal("scene_loaded", self)
 
 func updateChosenEquip(chosenEquip):
-	var equips = get_node("MainMenu/Choices/Equipment/Classes").get_children()
+	var equips = get_node("MainMenu/Choices/Class/Classes").get_children()
 	selected_equip = equips.find(chosenEquip)
 	for equip in equips:
 		if equip != chosenEquip:
@@ -70,7 +70,7 @@ func updateChosenProfile(chosenProfile):
 	checkIfCompleted()
 
 func checkIfCompleted():
-	if selected_character != -1 && selected_equip != -1 && get_node("MainMenu/Choices/CharacterName/Name").text.length() > 0 &&  get_node("MainMenu/Choices/Stats/Display/Remaining/Total").text == "0":
+	if selected_character != -1 && selected_equip != -1 && get_node("MainMenu/Choices/CharacterName/Name").text.length() > 0 &&  get_node("MainMenu/Choices/Stats/Display/Remaining/Total").text == "0" && gender != "":
 		get_node("MainMenu/Choices/Complete").disabled = false
 		get_node("MainMenu/Choices/Complete/FlashingText").color = Color(1, 1, 1)
 		get_node("MainMenu/Choices/Complete/FlashingText").state = FlashingText.States.FLASHING
@@ -78,7 +78,6 @@ func checkIfCompleted():
 		get_node("MainMenu/Choices/Complete").disabled = true
 		get_node("MainMenu/Choices/Complete/FlashingText").color = Color(0.7, 0.7, 0.7)
 		get_node("MainMenu/Choices/Complete/FlashingText").state = FlashingText.States.DISABLED
-
 
 func _on_Name_text_changed():
 	checkIfCompleted()
@@ -88,7 +87,8 @@ func _on_Complete_pressed():
 	player.name = get_node("MainMenu/Choices/CharacterName/Name").text
 	player.timeCreated = str(OS.get_unix_time())
 	player.classType = "PLAYER"
-	player.info = "New Character"
+	player.info = gender + " " + get_node("MainMenu/Choices/Class/Classes").get_children()[selected_equip].get_node("ClassName").text
+	player.gender = gender
 	
 	for stat in get_node("MainMenu/Choices/Stats/Display/Menu/SPEC").get_children():
 		player.stats[stat.name] = int(stat.get_node("Numbers/Number").text)
@@ -99,7 +99,7 @@ func _on_Complete_pressed():
 	player.picture.flip_profile = Characters.flip_profile[player.picture.path]
 	
 	CharacterManager.learn_attack(player, "melee", "punch")
-	var chosen_equip = get_node("MainMenu/Choices/Equipment/Classes").get_children()[selected_equip].get_node("ClassName").text
+	var chosen_equip = get_node("MainMenu/Choices/Class/Classes").get_children()[selected_equip].get_node("ClassName").text
 	if chosen_equip == "Knight":
 		chosen_equip = "knight"
 	if chosen_equip == "Battle Mage":
@@ -117,3 +117,31 @@ func _load_battle():
 	var CharacterSelection = load("res://Scenes/CharacterSelection/CharacterSelection.tscn").instance()
 	CharacterSelection._on_play_pressed()
 	queue_free()
+
+func _on_Male_mouse_entered():
+	if gender != "Male":
+		get_node("MainMenu/Choices/Gender/Male/MaleText").state = FlashingText.States.FLASHING
+
+func _on_Male_mouse_exited():
+	if gender != "Male":
+		get_node("MainMenu/Choices/Gender/Male/MaleText").state = FlashingText.States.DISABLED
+
+func _on_Male_pressed():
+	gender = "Male"
+	get_node("MainMenu/Choices/Gender/Male/MaleText").state = FlashingText.States.ENABLED
+	get_node("MainMenu/Choices/Gender/Female/FemaleText").state = FlashingText.States.DISABLED
+	checkIfCompleted()
+
+func _on_Female_mouse_entered():
+	if gender != "Female":
+		get_node("MainMenu/Choices/Gender/Female/FemaleText").state = FlashingText.States.FLASHING
+
+func _on_Female_mouse_exited():
+	if gender != "Female":
+		get_node("MainMenu/Choices/Gender/Female/FemaleText").state = FlashingText.States.DISABLED
+
+func _on_Female_pressed():
+	gender = "Female"
+	get_node("MainMenu/Choices/Gender/Female/FemaleText").state = FlashingText.States.ENABLED
+	get_node("MainMenu/Choices/Gender/Male/MaleText").state = FlashingText.States.DISABLED
+	checkIfCompleted()
