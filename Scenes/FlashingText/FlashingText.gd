7cb8b71e46d69e2783123ed60bc95baffff2e34f
eco9898 <eco9898@live.com>
2020-06-20 13:53:55 +1000

    Fix: Smoothed over the start of FlashingText animation
    Fix: Smoothed over the start of FlashingText animation
    The script now checks the previous state and plays an animation that starts at the opacity level

--- Scenes/FlashingText/FlashingText.gd
+++ Scenes/FlashingText/FlashingText.gd
@@ -16,28 +16,39 @@ export(States) var state = States.FLASHING setget set_state
 export var color := Color(1.0, 1.0, 1.0) setget set_color
 
 func set_state(value):
-	state = value
-	run_state()
+	run_state(value)
 
 func set_color(value):
 	set("custom_colors/font_color", value)
 
 func _ready():
-	run_state()
+	run_state(state)
 
-func run_state():
-	match state:
+func run_state(newState):
+	match newState:
 		States.FLASHING:
-			start_flashing()
+			if state == States.ENABLED:
+				start_flashing1()
+			elif state == States.DISABLED:
+				start_flashing()
+			elif state == States.HIDDEN:
+				start_flashing2()
 		States.ENABLED:
 			enable()
 		States.DISABLED:
 			disable()
 		States.HIDDEN:
 			hidden()
+	state = newState
 
 func start_flashing():
 	get_node("AnimationPlayer").play("Flashing")
+
+func start_flashing1():
+	get_node("AnimationPlayer").play("Flashing1")
+
+func start_flashing2():
+	get_node("AnimationPlayer").play("Flashing2")
 
 func enable():
 	get_node("AnimationPlayer").stop()
