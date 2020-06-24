bfd7fa1e1e69c81ae0479f719e486d7e6248666d
eco9898 <eco9898@live.com>
2020-06-24 11:20:41 +1000

    Resized from 720x1080 to 1440x2560
    Resized from 720x1080 to 1440x2560

--- Scenes/CharacterCreation/Profile.gd
+++ Scenes/CharacterCreation/Profile.gd
@@ -8,7 +8,7 @@ func _on_SelectButton_pressed():
 	if !chosen:
 		chosen = true
 		get_node("Border").show()
-		get_node("Pic").rect_position=Vector2(10,10)
-		get_node("Pic").rect_min_size=Vector2(180,180)
-		get_node("Pic").rect_size=Vector2(180,180)
+		get_node("Pic").rect_position=Vector2(20,20)
+		get_node("Pic").rect_min_size=Vector2(360,360)
+		get_node("Pic").rect_size=Vector2(360,360)
 		Core.get_parent().get_node("CharacterCreation").updateChosenProfile(self)
